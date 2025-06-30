73c2a92242823a58b3bdf1c7d5de3ae4a22b412e
Matheus Figueiredo <108003682+mfigueireddo@users.noreply.github.com>
2025-06-30 00:13:23 -0300

    Caso 4 reconhecido

--- saida/saida.py
+++ saida/saida.py
@@ -1,8 +1,4 @@
-dispositivo: {celular, movimento}
-dispositivo: {higromero, umidade}
-dispositivo: {lampada, potencia}
+dispositivo: {umidade}
 dispositivo: {Monitor}
-set potencia = 100 .
 se umidade < 40 entao enviar alerta Ar seco detectado
 	Monitor .
-se movimento == True entao ligar lampada senao desligar lampada .
